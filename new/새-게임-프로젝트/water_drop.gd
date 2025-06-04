f9c9ab3354e93792dedd0343e0c89800c24da593
Su-pport <88394816+Su-pport@users.noreply.github.com>
2025-06-05 00:01:13 +0900

    Delete new directory

--- new/새-게임-프로젝트/water_drop.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var deadline = global_position.y + 100
-
-func _process(delta: float) -> void:
-	global_position.y += 300*delta
-	if global_position.y == deadline :
-		queue_free()
